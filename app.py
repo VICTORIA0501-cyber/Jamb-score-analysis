8124f056579545a812a7fb491152a50d31267597
VICTORIA0501-cyber <victoriaolukoya05@gmail.com>
2025-01-24 09:58:33 +0000

    minor fixes

--- app.py
+++ app.py
@@ -1,16 +1,8 @@
 import streamlit as st
-from main import data_pel, grouped_data, df_jamb , grp_lbl
-import plotly.figure_factory as ff
+from main import data_pel, grouped_data 
 
 
 st.title("Welcome to ILARA/AKAKA")
 
 st.bar_chart(data_pel)
-
 st.bar_chart(grouped_data)
-
-fig = ff.create_distplot(df_jamb, grp_lbl)
-
-st.plotly_chart(fig, use_container_width=True)
-
-
